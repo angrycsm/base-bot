2cdab4b70aa00195d43253d7fc47401f774eaecc
biellofy <121532661+biellofy@users.noreply.github.com>
2023-07-21 14:48:07 +0000

    embedmeme.js

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-// Requer as classes discord.js necessárias
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { Client, Events, Collection, ActivityType, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
 
-// Criar uma nova instância para o client
-const client = new Client ({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
 
 client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-// Quando o cliente estiver pronto, execute este código (apenas uma vez)
-client.once(Events.ClientReady, c => {
-  console.log(`Conectado em ${c.user.tag}`);
-});
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
 
-// Logar no discord pelo token
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(`[PERIGO] O comando em ${filePath} está faltando uma propriedade "data" ou "execute" necessária.`)
+    }
+  }
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
 client.login(token);
